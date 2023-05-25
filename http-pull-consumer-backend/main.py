5792e60f3446aac00dcdbeda40001c6f262d5591
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-05-25 18:12:17 +0200

    ✨ Decode JWT contained in EndpointDataReference

--- http-pull-consumer-backend/main.py
+++ http-pull-consumer-backend/main.py
@@ -1,7 +1,9 @@
+import json
 import logging
 import pprint
 
 import coloredlogs
+import jwt
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -19,7 +21,33 @@ class EndpointDataReference(BaseModel):
     properties: dict
 
 
+def _decode_endpoint_data_ref(item: EndpointDataReference) -> dict:
+    ret = jwt.decode(item.authCode, options={"verify_signature": False})
+
+    ret["dad"] = json.loads(ret["dad"])
+
+    ret["dad"]["properties"]["authCode"] = jwt.decode(
+        ret["dad"]["properties"]["authCode"],
+        options={"verify_signature": False},
+    )
+
+    return ret
+
+
 @app.post("/")
 async def root(item: EndpointDataReference):
-    _logger.info("Received EndpointDataReference:\n%s", pprint.pformat(item.dict()))
+    _logger.info(
+        "Received %s:\n%s",
+        EndpointDataReference,
+        pprint.pformat(item.dict()),
+    )
+
+    decoded = _decode_endpoint_data_ref(item)
+
+    _logger.info(
+        "Decoded %s:\n%s",
+        EndpointDataReference,
+        pprint.pformat(decoded),
+    )
+
     return item
