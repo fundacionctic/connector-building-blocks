bcbed774278ad77b1ae643fb1349d11f7912ac6f
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-06-01 12:34:44 +0200

    ✨ Add event-driven API to mock

---
+++ datacellar-mock-component/event-api.py
@@ -0,0 +1,54 @@
+import asyncio
+import logging
+import os
+import random
+import json
+
+import coloredlogs
+from propan import PropanApp, RabbitBroker
+from propan.brokers.rabbit import RabbitExchange, RabbitQueue
+from pydantic import BaseModel
+
+coloredlogs.install(level=logging.INFO)
+_logger = logging.getLogger(__name__)
+
+broker = RabbitBroker(os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672/"))
+app = PropanApp(broker, logger=_logger)
+
+exchange = RabbitExchange("tasks-exchange", auto_delete=True)
+task_queue = RabbitQueue("aggregation-task-queue", auto_delete=True)
+
+
+class ConsumptionAggregationTaskMessage(BaseModel):
+    location: str
+    sink_url: str
+
+
+@broker.handle(task_queue, exchange)
+async def aggregation_handler(body: ConsumptionAggregationTaskMessage):
+    """Handler that listens for messages that act as triggers for long-running
+    tasks that compute aggregated electricity consumption datasets."""
+
+    _logger.info("Received electricity consumption aggregation task: %s", body)
+    sleep_time = random.random() * 5 + random.randint(1, 5)
+    _logger.info("Waiting for %s secs to simulate long-running task", sleep_time)
+    await asyncio.sleep(sleep_time)
+
+    _logger.info(
+        "Completed long-running task to compute aggregated electricity consumption: %s",
+        body,
+    )
+
+
+@app.after_startup
+async def send_mock_message_on_startup():
+    await asyncio.sleep(random.random() * 2)
+
+    await broker.publish(
+        queue="aggregation-task-queue",
+        exchange=exchange,
+        message=ConsumptionAggregationTaskMessage(
+            location="Bimenes",
+            sink_url="http://mock.datacellar.eu:8000/consumption/results",
+        ),
+    )
--- datacellar-mock-component/http-api.py
+++ datacellar-mock-component/http-api.py
@@ -7,6 +7,7 @@ from typing import List
 import arrow
 import coloredlogs
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 coloredlogs.install(level=logging.DEBUG)
@@ -81,3 +82,10 @@ async def get_consumption_data(location: str, day: date) -> ElectrictyConsumptio
     ]
 
     return ElectrictyConsumptionData(location=location, results=results)
+
+
+@app.get("/asyncapi.json", tags=["Event-driven API"])
+def get_asyncapi_schema():
+    """Returns the AsyncAPI schema that describes the mock event-driven API."""
+
+    return FileResponse("asyncapi.json")
