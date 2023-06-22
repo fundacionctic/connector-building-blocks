d0678ae1c4f2b53cdc8214a5f8433c9407ad369f
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-06-22 10:51:10 +0200

    Add examples for HTTP Push and listing assets

---
+++ example/example_http_push.py
@@ -0,0 +1,68 @@
+import asyncio
+import logging
+import os
+import pprint
+
+import coloredlogs
+
+from edcpy.config import ConsumerProviderPairConfig
+from edcpy.messaging import HttpPushMessage, start_messaging_app
+from edcpy.orchestrator import RequestOrchestrator
+
+_ASSET_ASYNCAPI_JSON = "GET-asyncapi-json"
+
+_SINK_BASE_URL = "http://consumer.local:8000"
+_SINK_PATH = "/push"
+_SINK_METHOD = "POST"
+
+_logger = logging.getLogger(__name__)
+_queue: asyncio.Queue[HttpPushMessage] = asyncio.Queue()
+
+
+async def push_handler(message: HttpPushMessage):
+    _logger.info(
+        "Putting HTTP Push request into the queue:\n%s", pprint.pformat(message.dict())
+    )
+
+    await _queue.put(message)
+
+
+async def main():
+    # Start the Rabbit broker and set the handler for the HTTP push messages
+    # received on the Consumer Backend from the Provider.
+    await start_messaging_app(http_push_handler=push_handler)
+
+    # The orchestrator contains the logic to interact with the EDC HTTP APIs.
+    # https://app.swaggerhub.com/apis/eclipse-edc-bot/management-api/0.1.0-SNAPSHOT
+    config = ConsumerProviderPairConfig.from_env()
+    orchestrator = RequestOrchestrator(config=config)
+    _logger.debug("Configuration:\n%s", pprint.pformat(config.__dict__))
+
+    transfer_details = await orchestrator.prepare_to_transfer_asset(
+        asset_query=_ASSET_ASYNCAPI_JSON
+    )
+
+    transfer_process = await orchestrator.create_provider_push_transfer_process(
+        contract_agreement_id=transfer_details.contract_agreement_id,
+        asset_id=transfer_details.asset_id,
+        sink_base_url=_SINK_BASE_URL,
+        sink_path=_SINK_PATH,
+        sink_method=_SINK_METHOD,
+    )
+
+    transfer_process_id = transfer_process["@id"]
+
+    await orchestrator.wait_for_transfer_process(
+        transfer_process_id=transfer_process_id
+    )
+
+    http_push_msg = await _queue.get()
+
+    _logger.info(
+        "Received response from Mock HTTP API:\n%s", pprint.pformat(http_push_msg.body)
+    )
+
+
+if __name__ == "__main__":
+    coloredlogs.install(level=os.getenv("LOG_LEVEL", "DEBUG"))
+    asyncio.run(main())
---
+++ example/example_list_assets.py
@@ -0,0 +1,25 @@
+import asyncio
+import logging
+import os
+import pprint
+
+import coloredlogs
+
+from edcpy.config import ConsumerProviderPairConfig
+from edcpy.orchestrator import CatalogContent, RequestOrchestrator
+
+_logger = logging.getLogger(__name__)
+
+
+async def main():
+    config = ConsumerProviderPairConfig.from_env()
+    orchestrator = RequestOrchestrator(config=config)
+    _logger.debug("Configuration:\n%s", pprint.pformat(config.__dict__))
+    catalog = await orchestrator.fetch_provider_catalog_from_consumer()
+    catalog_content = CatalogContent(catalog)
+    _logger.info("Found datasets:\n%s", pprint.pformat(list(catalog_content.datasets)))
+
+
+if __name__ == "__main__":
+    coloredlogs.install(level=os.getenv("LOG_LEVEL", "DEBUG"))
+    asyncio.run(main())
