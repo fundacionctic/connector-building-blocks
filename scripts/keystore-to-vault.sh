2f3bbbbd39a1ba3a1affc556871022b7db09635c
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-07-17 09:21:06 +0200

    🩹 Fix env var name and exit if vault exists

--- scripts/keystore-to-vault.sh
+++ scripts/keystore-to-vault.sh
@@ -4,12 +4,21 @@ set -e
 
 : "${KEYSTORE_PATH:?}"
 : "${KEYSTORE_PASSWORD:?}"
-: "${PATH_APP:?}"
+: "${PATH_CONNECTOR:?}"
+
+if [ -f ${PATH_CONNECTOR}/vault.properties ]; then
+    echo "File '${PATH_CONNECTOR}/vault.properties' already exists. Exiting..."
+    exit 0
+fi
 
 echo "Dumping certificate from keystore '${KEYSTORE_PATH}' to Vault properties file..."
 
-openssl pkcs12 -in ${KEYSTORE_PATH} -nodes -nokeys -passin pass:${KEYSTORE_PASSWORD} |
-    sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' >/tmp/cert.pem
+TEMP_DIR=$(python3 -c "import tempfile; print(tempfile.gettempdir());")/coreconn
 
-echo "publickey=$(cat /tmp/cert.pem)" >/tmp/vault.properties.temp
-sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\r\\n/g' /tmp/vault.properties.temp >${PATH_APP}/vault.properties
+rm -fr ${TEMP_DIR} && mkdir -p ${TEMP_DIR}
+
+openssl pkcs12 -in ${KEYSTORE_PATH} -nodes -nokeys -passin pass:${KEYSTORE_PASSWORD} |
+    sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' >${TEMP_DIR}/cert.pem
+
+echo "publickey=$(cat ${TEMP_DIR}/cert.pem)" >${TEMP_DIR}/vault.properties.temp
+sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\r\\n/g' ${TEMP_DIR}/vault.properties.temp >${PATH_CONNECTOR}/vault.properties
